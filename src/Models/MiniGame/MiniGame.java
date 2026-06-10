64074289b8e3f7aded7e40725251bcb285b5a6cc
SedHHN <sedhhn2@gmail.com>
2026-06-10 15:12:29 +0330

    student information

--- src/Models/MiniGame/MiniGame.java
+++
@@ -1,9 +0,0 @@
-package Models.MiniGame;
-
-import Models.GameMap;
-import Models.Seasons.Levels.Level;
-
-public class MiniGame extends Level{
-    public void onFirstTick();
-    public void onTick();
-}
