ae3873419648ad20d9e01ea75abdd3122762831c
Mahdi <mah8607di@gmail.com>
2026-06-24 16:23:29 +0330

    added some enums and comparing methods for leaderboard

--- src/Models/LeaderBoard/LeaderBoard.java
+++ src/Models/LeaderBoard/LeaderBoard.java
@@ -1,6 +1,37 @@
-import java.util.ArrayList;
 import Models.User.User;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class LeaderBoard {
-    public ArrayList<User> sortByScore(String sortType){return null;}
+
+    public ArrayList<User> sortUsers(List<User> users , LeaderBoardSortField sortField , SortType sortType) {
+        ArrayList<User> sortedUsers = new ArrayList<>(users);
+        Comparator<User> comparator = getComparator (sortField);
+
+        if (sortType == SortType.DESCENDING){
+            comparator = comparator.reversed();
+        }
+
+        sortedUsers.sort(comparator.thenComparing(User::getUsername));
+        return sortedUsers;
+    }
+
+    private Comparator<Users> getComparator(LeaderBoardSortField sortField){
+        return switch (sortField) {
+            case LAST_LEVEL_AND_SEASON -> Comparator
+                    .comparingInt(User::getLastUnlockedLevel)
+                    .thenComparingInt(User::getLastUnlockedSeason);
+            case MINI_GAMES_PASSED -> Comparator
+                    .comparingInt(User::getPassedMiniGamesCount)
+            case DAILY_QUESTS_COMPLETED -> Comparator
+                    .comparingInt(User::getCompletedDailyQuests);
+            case NON_DAILY_QUESTS_COMPLETED -> Comparator
+                .comparingInt(User::getCompletedNonDailyQuests);
+            case HIGHEST_SCORING_GAME_SCORE -> Comparator
+                    .comparingInt(User::getHighestScoringGameScore);
+        }
+    }
+
 }
