53d59522d971c1dfe7b54e30fba1c73ceda5e868
Yousof <ysfiran@gmail.com>
2026-07-10 00:10:42 +0330

    Finish: Setting Controller

--- app/src/main/java/pvz/Controller/user/SettingsController.java
+++ app/src/main/java/pvz/Controller/user/SettingsController.java
@@ -2,12 +2,19 @@ package pvz.Controller.user;
 
 import java.util.regex.Matcher;
 
+import pvz.Models.GameSession;
+import pvz.Utils.SaveManager;
 import pvz.View.MainMenu;
 import pvz.View.Menu;
 import pvz.View.Result;
 
 public class SettingsController {
-    public Result changeDifficulty(Matcher matcher) { return null; }
+    public Result changeDifficulty(Matcher matcher) { 
+        int newDifficulty = Integer.parseInt(matcher.group("difficulty"));
+        GameSession.getInstance().getCurrentUser().getSetting().setDifficulty(newDifficulty);
+        SaveManager.getInstance().save(GameSession.getInstance().getCurrentUser() , "users/" + GameSession.getInstance().getCurrentUser().getId() + ".json");
+        return new Result("changed difficulty to " + newDifficulty);  
+    }
     public Result exit(Matcher matcher) {
         Menu nextMenu = new MainMenu();
         return new Result("Exited to " + nextMenu.getName(), nextMenu);
