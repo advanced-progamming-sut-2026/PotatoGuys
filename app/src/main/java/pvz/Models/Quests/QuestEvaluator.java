8d254265986649fb6dec761ab5cd4d686708f9cb
Mahdi <mah8607di@gmail.com>
2026-07-20 21:47:51 +0330

    bug fixed

--- app/src/main/java/pvz/Models/Quests/QuestEvaluator.java
+++ app/src/main/java/pvz/Models/Quests/QuestEvaluator.java
@@ -4,7 +4,7 @@ import pvz.Models.Entities.Plants.Enums.PlantType;
 import pvz.Models.Games.GameContext;
 import pvz.Models.Games.GameStats;
 import pvz.Models.Games.map.GameMap;
-import pvz.Models.Games.map.Tile;
+import pvz.Models.Games.map.tile.Tile;
 import pvz.Models.User.User;
 
 public class QuestEvaluator {
