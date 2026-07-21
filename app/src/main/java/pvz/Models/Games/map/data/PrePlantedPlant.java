7d9586d71f230a496b4ab3c2af6df5c0c3987620
sedhhn <sedhhn@chmail.ir>
2026-07-21 21:20:33 +0330

    bug fix

---
+++ app/src/main/java/pvz/Models/Games/map/data/PrePlantedPlant.java
@@ -0,0 +1,9 @@
+package pvz.models.games.map.data;
+
+import pvz.models.entities.Plants.Enums.PlantType;
+
+public class PrePlantedPlant {
+    public PlantType type;
+    public int col;
+    public int lane;
+}
