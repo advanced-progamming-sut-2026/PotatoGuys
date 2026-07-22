c606e5b401f1fd020a4dd18b57d6d15adad2e8b7
Mahdi <mah8607di@gmail.com>
2026-07-22 20:16:30 +0330

    bug fixed

--- app/src/main/java/pvz/controller/LeaderBoardController.java
+++ app/src/main/java/pvz/controller/LeaderBoardController.java
@@ -15,14 +15,9 @@ public class LeaderBoardController {
     private SortTypes currentSortTypes = SortTypes.DESCENDING;
 
     public Result show(Matcher matcher) {
-<<<<<<< HEAD
-        List<LeaderBoardEntry> all = LeaderBoard.loadAll();
-        all = LeaderBoard.sort(all, currentField, currentSortType);
-        String output = LeaderBoard.format(all, currentField, currentSortType);
-=======
         List<LeaderBoardEntry> all = Leaderboard.loadAll();
+        all = Leaderboard.sort(all, currentField, currentSortTypes);
         String output = Leaderboard.format(all, currentField, currentSortTypes);
->>>>>>> ee9f758a11028279a7549dd7a76ab4fb05ff6b42
         return new Result(output);
     }
 
@@ -44,14 +39,9 @@ public class LeaderBoardController {
             }
         }
 
-<<<<<<< HEAD
-        List<LeaderBoardEntry> all = LeaderBoard.loadAll();
-        all = LeaderBoard.sort(all, currentField, currentSortType);
-        String output = LeaderBoard.format(all, currentField, currentSortType);
-=======
         List<LeaderBoardEntry> all = Leaderboard.loadAll();
+        all = Leaderboard.sort(all, currentField, currentSortTypes);
         String output = Leaderboard.format(all, currentField, currentSortTypes);
->>>>>>> ee9f758a11028279a7549dd7a76ab4fb05ff6b42
         return new Result(output);
     }
 
