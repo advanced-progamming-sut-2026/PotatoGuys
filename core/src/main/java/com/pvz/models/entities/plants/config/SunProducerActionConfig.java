aa111aac768331ec47717b353828772b8a9a7e74
Yousof <ysfiran@gmail.com>
2026-08-12 23:03:25 +0330

    Complete SunProducerAction

--- core/src/main/java/com/pvz/models/entities/plants/config/SunProducerActionConfig.java
+++ core/src/main/java/com/pvz/models/entities/plants/config/SunProducerActionConfig.java
@@ -5,9 +5,13 @@ import com.pvz.models.entities.plants.data.ProductionKind;
 
 public class SunProducerActionConfig extends PlantActionConfig {
 
+    /** Seconds between production cycles (0 = produce immediately / one-shot). */
+    public float intervalSeconds;
     public ProductionKind productionKind;
     public float amount;
     public float[] amounts;
     public float[] stageIntervalSeconds;
     public Vector2 spawnOffset = new Vector2();
+    /** Sun pops out this many seconds into the attack/feed animation (defaults to 0 = instantly). */
+    public float delaySeconds = 0f;
 }
