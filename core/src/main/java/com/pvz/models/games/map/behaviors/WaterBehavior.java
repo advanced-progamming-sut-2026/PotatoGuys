3bf6aea4889c05deffa50ac663c3351e5f00d8a9
sedhhn <sedhhn@chmail.ir>
2026-08-17 22:35:47 +0330

    water added to Big Wave Beach

--- core/src/main/java/com/pvz/models/games/map/behaviors/WaterBehavior.java
+++ core/src/main/java/com/pvz/models/games/map/behaviors/WaterBehavior.java
@@ -1,14 +1,22 @@
 package com.pvz.models.games.map.behaviors;
 
+import com.badlogic.gdx.math.Vector2;
+import com.pvz.controller.game.GameController;
+import com.pvz.models.engine.FrameConfig;
 import com.pvz.models.entities.plants.Plant;
 import com.pvz.models.entities.plants.data.PlantPropertySheet;
 import com.pvz.models.entities.plants.data.PlantRegistry;
 import com.pvz.models.entities.plants.enums.PlantTag;
 import com.pvz.models.entities.plants.enums.PlantType;
+import com.pvz.models.games.GameContext;
 import com.pvz.models.games.card.PlantCard;
 import com.pvz.models.games.map.tile.Tile;
 
 public class WaterBehavior implements TileBehavior {
+    private static final String PAM_PATH = "768/FULL/BACKGROUNDS/WATER_SQUARE/WATER_SQUARE.PAM";
+    private static final String CLIP = "Water";
+
+    float stateTime;
 
     @Override
     public boolean canPlant(PlantCard card, Tile tile) {
@@ -28,6 +36,20 @@ public class WaterBehavior implements TileBehavior {
         }
 
         return sheet.getTags().contains(PlantTag.STACK);
+    }
+
+    @Override
+    public void update(GameContext ctx, Tile tile, float dt) {
+        TileBehavior.super.update(ctx, tile, dt);
+        stateTime += dt;
+    }
+
+    @Override
+    public FrameConfig draw(Tile tile) {
+        TileBehavior.super.draw(tile);
+        Vector2 pos = new Vector2(GameController.colToWorldX(tile.getCol()), GameController.laneToWorldY(tile.getLane()));
+        Vector2 scale = new Vector2(0.65f, 0.65f);
+        return new FrameConfig(PAM_PATH, CLIP, stateTime, pos, scale, null, true);
     }
 
     @Override
