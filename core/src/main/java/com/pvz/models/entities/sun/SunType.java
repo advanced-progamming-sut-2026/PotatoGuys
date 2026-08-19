c6039f72bccbedf295793d760cf3afa31cc4e83d
sedhhn <sedhhn@chmail.ir>
2026-08-19 19:37:33 +0330

    Rebalance in sun type values

--- core/src/main/java/com/pvz/models/entities/sun/SunType.java
+++ core/src/main/java/com/pvz/models/entities/sun/SunType.java
@@ -1,16 +1,17 @@
 package com.pvz.models.entities.sun;
 
 public enum SunType {
-    NORMAL(25),
-    SPECIAL(100),
-    RADIOACTIVE(25);
+    NORMAL(50),
+    SPECIAL(150),
+    RADIOACTIVE(50);
 
     private final int amountSun;
-    private SunType(int amountSun){
+
+    private SunType(int amountSun) {
         this.amountSun = amountSun;
     }
 
-    public int getAmountSun(){
+    public int getAmountSun() {
         return this.amountSun;
     }
 
